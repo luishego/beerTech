50f98723fcdd6bc7819986a8cf14d2b092ee7f26
luishego <luisherglz@gmail.com>
2018-02-16 10:48:03 -0600

    first prototype of the arduino/python tmeperature sensor with audible alarm

---
+++ temp_001.py
@@ -0,0 +1,39 @@
+import serial 
+import numpy
+import matplotlib.pyplot as plt
+from drawnow import *
+import pygame
+from pygame import *
+
+timeS = []
+tempC = []
+arduinoData = serial.Serial('com12', 9600)
+plt.ion()
+pygame.mixer.init()
+
+def makeFig():	
+	plt.plot(tempC, 'ro-', label='Centigrados')
+	plt.legend(loc='upper left')
+	
+
+
+while True: 
+	while(arduinoData.inWaiting()==0):
+		pass
+	arduinoString = (arduinoData.readline().decode("utf-8"))
+	dataArray = arduinoString.split(',')
+	time = float (dataArray[0])
+	temp = float (dataArray[1])
+	timeS.append(time)
+	tempC.append(temp)
+	print (timeS,tempC)
+	drawnow(makeFig)
+	plt.pause(0.000001)
+	
+
+	if temp >= 25:
+		pass
+		pygame.mixer.music.load('alarm1.ogg')
+		pygame.mixer.music.play();
+
+
